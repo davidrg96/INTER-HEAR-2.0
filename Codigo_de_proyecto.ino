1ed4a7f3f2fdbfa2532878a052be50fcafa94220
davidrg96 <davidramirez_upci@outlook.com>
2020-09-24 21:14:49 -0500

    Create Codigo_de_proyecto.ino

---
+++ Codigo_de_proyecto.ino
@@ -0,0 +1,11 @@
+//This one is the first part our proyect, we going to use Arduino, Labview, maybe proteus and visual studio. Here we are going to put or change somethings about our project,
+//if we are going to use another software, we put a label to say it the part that is in the other software.
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
